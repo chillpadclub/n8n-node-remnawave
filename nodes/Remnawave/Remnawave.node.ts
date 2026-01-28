3a2e84abadc87b18e02eab325440c655e5299b9c
chillpad <k@chillpad.club>
2026-01-28 16:23:16 +0300

    modified:   nodes/Remnawave/Remnawave.node.ts

--- nodes/Remnawave/Remnawave.node.ts
+++ nodes/Remnawave/Remnawave.node.ts
@@ -12,7 +12,7 @@ export class Remnawave implements INodeType {
 		name: 'remnawave',
 		group: ['transform'],
 		version: 1,
-		icon: 'file:Remnawave.node.png',
+		icon: 'fa:server',
 		description: 'Interact with the Remnawave API',
 		defaults: {
 			name: 'Remnawave',
